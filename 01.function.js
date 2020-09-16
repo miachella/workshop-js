2f1bb2e0cfbe75c20f70454367b514e9a0207fb4
Mathilde IACHELLA <mathilde.iachella@gmail.com>
2020-09-16 10:10:26 +0200

    TP 01 - Les fonctions

---
+++ 01.function.js
@@ -0,0 +1,32 @@
+console.log("01 - Fonctions");
+
+// Définition d’une fonction
+var nombre1 = 10; 
+var nombre2 = 20;
+function additionner(nb1, nb2) {
+    return nb1 + nb2;
+} 
+
+var resultat1 = additionner(nombre1, nombre2);
+console.log("resultat1 = " + resultat1);
+
+//Variable de type fonction
+var somme = additionner;
+var resultat2 = somme(nombre1, nombre2);
+console.log("resultat2 = " + resultat2);
+
+var multiplication = function(nb1, nb2) {
+    return nb1 * nb2;
+}
+var resultat3 = multiplication(nombre1, nombre2);
+console.log("resultat3 = " + resultat3);
+
+//Fonction comme élément du 1er ordre
+var afficherOperation = function(nomOperation, operation, nb1, nb2) {
+    return nomOperation + "(" + nb1 + "," + nb2 + ") = " + operation(nb1, nb2);
+}
+
+console.log(afficherOperation("Somme", somme, 20, 40));
+console.log(afficherOperation("Multiplication", multiplication, 10, 20))
+console.log(afficherOperation("Soustraction", function(nb1, nb2) {return nb1 - nb2}, 15, 5))
+
